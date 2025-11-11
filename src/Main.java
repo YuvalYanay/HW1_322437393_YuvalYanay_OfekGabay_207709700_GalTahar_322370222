22818a911bb35e6ec6758ef410d3b14d1ab42ce1
Yuval Yanay <Yuval Yanay@10.100.102.128>
2025-11-11 16:48:13 +0200

    This is commit 3. Phase 3 - Finishing all the classes, besides the main.

--- src/Main.java
+++ src/Main.java
@@ -9,42 +9,10 @@ public class Main {
 		
 		Scanner s = new Scanner(System.in);
 		
-		try {
-			File f = new File("FN","FT"); //File object
-			
-			ArrayList<File> files = new ArrayList<File>(); //Array of files
-			
-			files.add(f); //Adding the files into the array
-			
-			for(File file : files) { //Printing the files' data
-				
-				System.out.println(f.toString());
-				
-				
-			}
-			
-			
-			Message m = new BoardMessage("Yuval","Hello my name is Yuval Yanay and I live in Nordiya.",true);
-			
-			ArrayList<Message> messages = new ArrayList<Message>();
-			
-			messages.add(m);
-			
-			for(Message message : messages) {
-				
-				if (message instanceof BoardMessage) {
-					BoardMessage messageBoard = (BoardMessage) message;
-					System.out.println(message.toString());
-
-				}
-				
-				
-			}
-			
-		}
-		catch(IllegalArgumentException e) {
-			System.out.println("Exception: " + e.getLocalizedMessage()); 
-		}
+		ArrayList<Message> messages = new ArrayList<Message>();
+		
+		Message messBoard = new BoardMessage("Yuval","Hi, my name is Yuval and I live in Nordiya",false);
+		Message meesEmail = new EmailMessage("Ofek", "Hi, I'm ofek. This is an important email regarding tax issues.", true, "Tax issues", PriorityType.URGENT);
 		
 		
 	}
